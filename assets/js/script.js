665b7c804f273f943dcf0ce387d47aa49ad70ac2
FabricioSilvany <fabriciosilvany57@gmail.com>
2025-05-23 18:01:45 +0000

    Concluído

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ const key = '5d9fef7547334ec8f99ccbe6fd8e8a32'
 
 //eventos
 
+
 botao.addEventListener('click',buscar_cidade)
 
 
 //função
+
 async function buscar_cidade(){
     
     procura_cidade = dcidade.value
@@ -21,5 +23,10 @@ async function buscar_cidade(){
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${procura_cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
 
     console.log(dados)
+
+    cidade.textContent = `Tempo em ${dados.name}`
+    temperatura.textContent = `${(dados.main.temp).toFixed(0)}°C`
+    previsao.textContent = `${dados.weather[0].description}`
+    icone.src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 
 }
